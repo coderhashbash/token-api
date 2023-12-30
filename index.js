2074af1d79ec2c171b5adcb8a711a29d3ee236f6
coderhashbash <149156362+coderhashbash@users.noreply.github.com>
2023-12-30 16:41:06 +0530

    2nd

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/api/live-price', (req, res) => {
 });
 
 // Start the server
-const port = 3001;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
